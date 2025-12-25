eeebf4acebcf2e24d39aa1eb8c84916772cf1a99
samtech85 <herlytache@gmail.com>
2025-12-25 10:06:09 -0500

    title modified

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import "../css/Navbar.css";
 function NavBar() {
     return <nav className="navbar">
         <div className="navbar-brand">
-            <Link to="/" className="brand-title">Movie App</Link>
+            <Link to="/" className="brand-title">My Movie App</Link>
         </div>
         <div className="navbar-links">
             <Link to="/" className="nav-link">Home</Link>
